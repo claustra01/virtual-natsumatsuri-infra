2bc13ea47d17afe167ae4ef4e45a3fd8b3d6c316
claustra01 <shinya430.g@gmail.com>
2024-08-05 20:49:12 +0900

    fix: variables format

--- terraform/artifact_registry.tf
+++ terraform/artifact_registry.tf
@@ -1,6 +1,6 @@
 resource "google_artifact_registry_repository" "default" {
   repository_id = "${var.project_id}"
-  location      = var.region
+  location      = "${var.region}"
   format        = "DOCKER"
 
   cleanup_policies {
