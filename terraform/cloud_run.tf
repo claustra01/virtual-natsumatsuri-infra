8f43272c9e3562b31470289922f3f4cd98737d2e
claustra01 <shinya430.g@gmail.com>
2024-08-05 20:17:21 +0900

    add: cloud run iam member

--- terraform/cloud_run.tf
+++ terraform/cloud_run.tf
@@ -12,6 +12,13 @@ resource "google_cloud_run_v2_service" "default" {
   }
 }
 
+resource "google_cloud_run_survice_iam_member" "member" {
+  service = google_cloud_run_v2_service.default.name
+  location = google_cloud_run_v2_service.default.location
+  role = "roles/run.invoker"
+  member = "serviceAccount:terraform@${var.project_id}.iam.gserviceaccount.com"
+}
+
 resource "google_cloud_run_service_iam_binding" "noauth" {
   service  = google_cloud_run_v2_service.default.name
   location = google_cloud_run_v2_service.default.location
