d78f96bf9356c242b658aeab33628a64424293af
claustra01 <shinya430.g@gmail.com>
2024-08-05 21:30:57 +0900

    wip: test

--- terraform/cloud_run.tf
+++ terraform/cloud_run.tf
@@ -1,6 +1,6 @@
 resource "google_cloud_run_v2_service" "default" {
-  name     = "${var.service_name}"
-  location = "${var.region}"
+  name     = var.service_name
+  location = var.region
 
   template {
     containers {
